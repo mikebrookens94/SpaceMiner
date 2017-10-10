34cccbc3267cdb41b8b257ad3e9ec584b84d4701
Caiggas <Caiggas@gmail.com>
2017-10-10 19:08:37 -0400

    added a player object and basic 3d movement.
    full 2d movement including mouselook and rotational control.

---
+++ Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour {
+
+    public float moveSpeed;
+    public float rotateSpeed;
+    public float mouseSensitivity;
+
+    private Rigidbody playerRB;
+
+	void Start () {
+        playerRB = GetComponent<Rigidbody>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	void FixedUpdate () {
+        //takes player input.
+        float moveX = Input.GetAxis("X-axis");
+        float moveY = Input.GetAxis("Y-Axis");
+        float moveZ = Input.GetAxis("Z-axis");
+
+        float rotateX = Input.GetAxis("Mouse Y") * mouseSensitivity;
+        float rotateY = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float rotateZ = Input.GetAxis("Z-rotaxis");
+
+        //sets vector3s from input to give to the player oject
+        Vector3 movement = new Vector3(moveX, moveY, moveZ);
+        Vector3 rotation = new Vector3(rotateX,rotateY,rotateZ);
+
+        //makes player object move
+        playerRB.AddRelativeForce(movement * moveSpeed);
+        gameObject.transform.Rotate(rotation);
+
+    }
+}
