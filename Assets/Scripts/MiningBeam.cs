7686516e4bb7b7ba878235d79a8280722dd1434a
Caiggas <Caiggas@gmail.com>
2017-10-11 17:39:45 -0400

    Mining Mechanics
    made asteroids disappear on raytrace and incriment an "ore quantity" counter.

---
+++ Assets/Scripts/MiningBeam.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiningBeam : MonoBehaviour {
+
+    public float mineRange;
+
+    private float oreQuantity = 0f;
+    private GameObject toMine;
+    private Rigidbody toMineRB;
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        RaycastHit hit;
+        Ray miningRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0)) {
+            if (Physics.Raycast(miningRay, out hit, mineRange)) {
+                toMine = hit.transform.gameObject;
+                toMineRB = GetComponent<Rigidbody>();
+                oreQuantity += toMineRB.mass;
+                GameObject.Destroy(toMine);
+                Debug.Log(oreQuantity);
+            }
+        }
+	}
+}
